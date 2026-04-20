6327cf62bb6486367498d5923ea96296635a8e4a
John Lin <120153300+codecreator127@users.noreply.github.com>
2026-04-20 19:03:13 +1200

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        ## use a complement approach
+        ## one pass through, return if we see the complement
+        ## use hashmap to store the seen nums and their index
+
+        seen = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in seen:
+                return [seen[target - nums[i]], i]
+
+            seen[nums[i]] = i
