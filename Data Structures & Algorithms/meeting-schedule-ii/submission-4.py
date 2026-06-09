f5465c6a5aef85f28783fc23feb48efb78f7622b
John Lin <120153300+codecreator127@users.noreply.github.com>
2026-06-09 20:34:49 +1200

    Add: meeting-schedule-ii - submission-4

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-4.py
@@ -0,0 +1,39 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        ### maintain a minheap of intervals by end time
+        ### sort by start time, iterate through the intervals
+        ## if we find an overlap, then we open a new room
+        ## maintain the num of rooms using a minheap, just check top of heap (earliest time) for overlap
+
+        import heapq
+
+        ## sort by start time
+        intervals.sort(key=lambda x: x.start)
+
+        heap = []
+
+        rooms = 0
+
+        for i in range(len(intervals)):
+            curr = intervals[i]
+
+            ## check overlap
+            if heap and curr.start < heap[0][0]:
+                heapq.heappush(heap, (curr.end, curr.start))
+            else:
+                ## no overlap, then we can evict all rooms that have ended
+                while heap and heap[0][0] <= curr.start:
+                    heapq.heappop(heap)
+                heapq.heappush(heap, (curr.end, curr.start))
+
+            rooms = max(rooms, len(heap))
+
+        return rooms
